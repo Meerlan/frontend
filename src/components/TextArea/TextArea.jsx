d92537340c78685a510dc153132bcb77e9ad7679
Admin <admin@Meerlan-Pro.local>
2023-11-02 15:05:35 +0600

    textarea updated

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -10,15 +10,17 @@ class Textarea extends Component {
     };
   }
 
-  getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+ 
 
+  handleChange = (event) => {
+    const newText = event.target.value;
+    const bgColor = this.getRandomColor();
+
+    this.setState({
+      text: newText,
+      bgColor: bgColor,
+    });
+  };
 
   render() {
     const { text, bgColor } = this.state;
